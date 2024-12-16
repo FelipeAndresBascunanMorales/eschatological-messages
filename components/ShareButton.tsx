abf0efd255f7ba3216034776d64144250046c63f
Felipe Bascuñán <f.bascunan03@gmail.com>
2024-12-16 20:35:53 -0300

    feat: motivation-app-3

--- components/ShareButton.tsx
+++ components/ShareButton.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { FacebookShareButton, TwitterShareButton, LinkedinShareButton } from 'react-share';
+import { FacebookShareButton, TwitterShareButton, EmailShareButton } from 'react-share';
 
 interface ShareButtonProps {
   message: string;
 }
 
 const ShareButton: React.FC<ShareButtonProps> = ({ message }) => {
-  const encodeMessage = encodeURIComponent(message);
   const shareUrl = window.location.href;
 
   return (
-    <div>
+    <div className="share-buttons">
       <FacebookShareButton url={shareUrl} quote={message}>Share on Facebook</FacebookShareButton>
       <TwitterShareButton url={shareUrl} title={message}>Share on Twitter</TwitterShareButton>
-      <LinkedinShareButton url={shareUrl} title={message}>Share on LinkedIn</LinkedinShareButton>
+      <EmailShareButton url={shareUrl} subject="Daily Motivation" body={message}>Share via Email</EmailShareButton>
     </div>
   );
 };
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,18 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import MessageDisplay from '../components/MessageDisplay';
 import ShareButton from '../components/ShareButton';
-import './Home.css';
 
 const Home: React.FC = () => {
-  const [message, setMessage] = useState<string>('Stay positive, work hard, make it happen!');
-  const [backgroundColor, setBackgroundColor] = useState<string>('#f0e68c');
-
-  useEffect(() => {
-    // Additional logic to fetch message and color can be added here
-  }, []);
+  const message = "Stay positive and keep pushing forward!";
+  const backgroundColor = "#f0f8ff";
 
   return (
-    <div className='home'>
+    <div className="home-container">
       <MessageDisplay message={message} backgroundColor={backgroundColor} />
       <ShareButton message={message} />
     </div>
